d9493417bc29aac0a5bd025897de4d11420e934e
Daniel K <daniel-gk@hotmail.com>
2023-07-09 10:39:26 -0300

    Optimize prime number generation and limit values
    Highest number limited to 99999999 due to performance implications

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -43,11 +43,17 @@ function formData() {
   let maxRoll = parseInt(formData.get('max_roll'))
   let numberRoll = parseInt(formData.get('num_roll'))
 
-  display = document.querySelector('.num_roll')
+  displayMax = document.querySelector('.max_roll')
+  displayNumber = document.querySelector('.num_roll')
+
+  if (maxRoll > 99999999){
+    maxRoll = 99999999
+    displayMax.value = maxRoll
+  }
 
   if (maxRoll < numberRoll) {
     numberRoll = maxRoll
-    display.value = maxRoll
+    displayNumber.value = maxRoll
     alert(`'number of rolls' > 'highest number'`)
   }
 
@@ -74,14 +80,11 @@ function randomNumber(max, number) {
     }
   } else if (focus === 'prime') {
     base.push(2)
-    for (let i = 3; i <= max; i++) {
-      for (let j = 0; j < base.length; j++) {
-        if (i % base[j] === 0) {
-          break
-        }
-        if (j === base.length - 1) {
-          base.push(i)
-        }
+    for (let i = 3; i <= max; i += 2) {
+      if (i === 3 || i === 5 || i ===7){
+        base.push(i)
+      }else if (i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0) {
+        base.push(i)
       }
     }
   } else if (focus === 'fibonacci') {
